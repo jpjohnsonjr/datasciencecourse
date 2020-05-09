a336e439873ea6d7c900e0df03a43f9396e17cd8
J.P. Johnson Jr <jpjohnsonjr@outlook.com>
2020-05-09 15:39:14 -0400

    Looking to see what can and can't be done within this window.

--- TestingR.R
+++ TestingR.R
@@ -3,3 +3,5 @@ print("This fiel was created within RStudio...")
 print("... and now it lives on GitHub.")
 
 print("Making a change to it just to test out.")
+
+print("Looking to see what can be done within the 'Commit' window.")
